886de69c23c118d33a83ee39eb61a093b042c4a3
Min <125743623+min-04A@users.noreply.github.com>
2025-04-10 11:30:27 -0400

    first commit: create necessary methods

---
+++ Client.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Client
+{
+    private Socket socket;
+
+    public Client(String host, int port)
+    {
+
+    }
+
+    public Socket getSocket()
+    {
+        return this.socket;
+    }
+
+    public void handshake()
+    {
+        
+    }
+
+    public void disconnect()
+    {
+        
+    }
+}
---
+++ Server.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Server 
+{
+    public void serve(int expectedClients)
+    {
+        
+    }
+}

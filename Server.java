7fb11adca0dcbde6fed894d0edeb0a960ef5700f
Min <125743623+min-04A@users.noreply.github.com>
2025-04-11 14:53:59 -0400

    second commit: send/receive messages

--- Server.java
+++ Server.java
@@ -4,8 +4,25 @@ import java.io.*;
 
 public class Server 
 {
+    private ServerSocket serverSocket;
+
     public void serve(int expectedClients)
     {
-        
+        // Handle all the clients given
+        for(int i=0; i<expectedClients; i++)
+        {
+            try
+            {
+                Socket clientSocket = serverSocket.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            }
+
+            // exit server if exception
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
